9a42e31b0fb2e512a1a7b71b77fd52f81f788866
Mark Struberg <struberg@apache.org>
2012-08-25 21:42:39 +0200

    small test project for incremential build tests

---
+++ moduleA/src/main/java/BeanA.java
@@ -0,0 +1,15 @@
+/**
+ * dumb test bean
+ */
+public class BeanA {
+
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
---
+++ moduleB/src/main/java/BeanB.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * another dumb test bean
+ */
+public class BeanB {
+    private int j;
+    private BeanA a;
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+}
